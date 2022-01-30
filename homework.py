d87dd27cb31986f2359500b0dc3ea4eb17389283
Ramil Shafikov <rshafikov.work@yandex.ru>
2022-01-30 21:44:51 +0300

    seventh try

--- homework.py
+++ homework.py
@@ -70,7 +70,9 @@ class Running(Training):
     MINUTES_IN_HOUR: int = 60
 
     def get_spent_calories(self) -> float:
-        return ((self.COEFF_CALORIE_1 * self.get_mean_speed() - self.COEFF_CALORIE_2)
+        return ((self.COEFF_CALORIE_1
+                * self.get_mean_speed()
+                - self.COEFF_CALORIE_2)
                 * self.weight
                 / self.M_IN_KM
                 * self.duration
@@ -93,7 +95,7 @@ class SportsWalking(Training):
         super().__init__(action, duration, weight)
         self.height = height
 
-    def get_spent_calories(self) -> float:    
+    def get_spent_calories(self) -> float:
         return ((self.COEFF_CALORIE_1 * self.weight
                 + (self.get_mean_speed()**self.COEFF_CALORIE_3 // self.height)
                 * self.COEFF_CALORIE_2 * self.weight)
